633e1e7abd6bd54e8b2254473835f769ed2eb853
taha2oo2 <106246522+taha2oo2@users.noreply.github.com>
2022-11-29 22:02:25 +0500

    Update text.js

--- text.html/text.js
+++ text.html/text.js
@@ -1,10 +1,16 @@
 <html>
 <input type="text" id="text1">
 <button type="button" onmouseover="button()"  >Enter</button>
+
+<button type="submit" onkeypress="but()"  >Submit</button>
 </body>
 </html>
 function button(){
  var a = document.getElementById("text1");
  alert(a.value);
  console.log(a.value);
+}
+function but(){
+    var a = document.getElementById("text1");
+    alert(a.val="Hello!");
 }
